302d6f9c96fb4501cc37f8e5e42ab3290a89b87c
Monte-Christo-Edmon-Dantes <terius20@naver.com>
2025-09-18 22:21:54 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// 메뉴 클릭 시 부드럽게 이동
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
